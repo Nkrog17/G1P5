bd9193b5f811ffd3122bc0cb6e44ffe4146d9ecd
Emil Pettersson <epette17@student.aau.dk>
2019-12-02 14:15:17 +0100

    Addede animation, dissolve shader, script der styrer dissolve shader og objekts

--- Forest/Assets/Shaders/ExampleAssets/ShaderCont.cs
+++ Forest/Assets/Shaders/ExampleAssets/ShaderCont.cs
@@ -5,53 +5,104 @@ using UnityEngine;
 public class ShaderCont : MonoBehaviour
 {
     float timer = 0f;
-    public float SliceAmount = 0.0f;
-    //public Renderer[] rend;
-    public Renderer rend;
+    public Renderer[] rend;
+    //public Renderer rend;
     float amt;
-    bool amt1;
-    public GameObject objectName;
-   
+    bool amt1 = false;
+    public GameObject[] prop;
+    int[] timerArray = new int[10] { 5, 20, 30, 60, 80, 100, 120, 140, 160, 180 };
+    public static int counter = 0;
+    public static bool setInactive = false;
+    public static bool stopCounter = false;
+
     Material mat;
 
     // Start is called before the first frame update
     void Start()
     {
-        mat = rend.material;
-        objectName.SetActive(false);
+  
+        //amt = burneffekt hvor 0 = objekt er visibel og 1 = objekt er brændt væk
         amt = 1;
     }
-    
+
 
     // Update is called once per frame
     void Update()
     {
+        if (!setInactive)
+        {
+            propActiveStatus();
+            
+        }
+
         timer += Time.deltaTime;
         //amt = mat.GetFloat("_SliceAmount");
+
+
+        //Debug.Log(timer);
+
+        mat = rend[counter].material;
         
-
-        Debug.Log(timer);
-          if (timer >= 5 && timer < 10 && !amt1){
-            objectName.SetActive(true);
-
-            amt -= 0.01f;
-            mat.SetFloat("_SliceAmount", amt);
-            if (amt <= 0)
-            {
-                amt1 = false;
-            }
-        }
-          
-          if (timer >= 10 && amt1 == false)
+        if (timer >= timerArray[counter] && timer < (timerArray[counter] + 5) && !amt1)
         {
-            amt += 0.01f;
-            mat.SetFloat("_SliceAmount", amt);
-            if (amt >= 1)
+            propCall(counter);
+        }
+
+        if (timer >= timerArray[counter]+5 && !amt1)
+        {
+            propKill(counter);
+        }
+
+        if (counter > rend.Length-1)
+        {
+            counter = rend.Length - 1;
+            stopCounter = true;
+        }
+    }
+
+
+
+    void propCall(int i)
+    {
+        prop[i].SetActive(true);
+
+        amt -= 0.02f;
+        mat.SetFloat("_SliceAmount", amt);
+        if (amt <= 0)
+        {
+            amt1 = false;
+            amt = 0;
+        }
+    }
+
+    void propKill(int i)
+    {
+        amt += 0.02f;
+        //Ændrer sliceamount i dissolve2nd shaderen.    
+        mat.SetFloat("_SliceAmount", amt);
+        if (amt >= 1)
+        {
+            //amt1 = true;
+            amt = 1;
+            if (!stopCounter)
             {
-                amt1 = true;
+               counter++;
+            }
+           
+            Debug.Log("Counter is = " + counter);
+        }
+    }
+    void propActiveStatus()
+    {
+        for (int i = 0; i < rend.Length; i++)
+        {
+            prop[i].SetActive(false);
+            if (i == rend.Length-1)
+            {
+                setInactive = true;
             }
         }
-        
-                
     }
 }
+
+
